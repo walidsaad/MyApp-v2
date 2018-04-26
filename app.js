43d0f4e649d08ab69f0ab3249e1950a5ff76ce30
walidsaad <walid.saadd@gmail.com>
2018-04-26 23:03:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ app.controller('myCtrl', function($http,$scope)
 		{
 		
 		$scope.afficher=function(){
-		$http.get('listeetudiantsJSON.php')
+		$http.get('http://41.229.35.212/MyApp-v2/listeetudiantsJSON.php')
 		  .success(function(response){
                     $scope.etudiants=[];
 		    console.log(response);
